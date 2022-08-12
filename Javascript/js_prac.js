678f933390abe01ca832eebc57936be9fb8a234f
Your Name <you@example.com>
2022-08-12 18:36:23 +0530

    Javascript 1st practice program

---
+++ Javascript/js_prac.js
@@ -0,0 +1,21 @@
+/* if condition
+
+name=prompt("enter your name: ")
+age=prompt("enter your age: ")
+if(age>=18)
+{
+    alert("hii, "+name+" is  eligible for vote")
+}
+else{
+    alert("hii, "+name+" is not eligible for vote")
+} 
+
+*/
+var na="surendran"
+console.log("leng =  "+na.length)
+for(var i=0;i<na.length;i++)
+{
+  
+    console.log("char = "+na[i])
+
+}
